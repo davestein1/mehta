37d30b875dd11a03e196ba3a08268052a578b23a
David Stein <github@steinbay.com>
2015-07-05 22:47:29 -0700

    copyright and credit

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-			<?php hybrid_get_sidebar( 'primary' ); // Loads the sidebar/primary.php template. ?>
+		<?php hybrid_get_sidebar( 'primary' ); // Loads the sidebar/primary.php template. ?>
 
 		</div><!-- #main -->
 
@@ -6,30 +6,26 @@
 
 		<?php hybrid_get_menu( 'subsidiary' ); // Loads the menu/subsidiary.php template. ?>
 
-<footer <?php hybrid_attr( 'footer' ); ?>>
+		<footer <?php hybrid_attr( 'footer' ); ?>>
 
-	<div class="footer_wrap">
-
-		<?php wp_nav_menu(
-			array(
-				'menu'            => 'footer',
-				'container'       => 'nav',
-				'container_id'    => 'menu-footer',
-				'container_class' => 'menu',
-				'menu_id'         => 'menu-footer-items',
-				'menu_class'      => 'menu-items',
-				'fallback_cb'     => '',
-				'items_wrap'      => '<div class="wrap"><ul id="%s" class="%s">%s</ul></div>'
-			)
-		); ?>
+			<?php wp_nav_menu(
+				array(
+					'menu'            => 'footer',
+					'container'       => 'nav',
+					'container_id'    => 'menu-footer',
+					'container_class' => 'menu',
+					'menu_id'         => 'menu-footer-items',
+					'menu_class'      => 'menu-items',
+					'fallback_cb'     => '',
+					'items_wrap'      => '<div class="wrap"><ul id="%s" class="%s">%s</ul></div>'
+				)
+			); ?>
 		
-		<p class="credit">
-			<?php do_action('jh_credit'); ?>
-		</p><!-- .credit -->
-
-	</div><!-- .footer_wrap -->
-
-</footer><!-- #footer -->
+			<div class="footer_wrap">
+				<?php do_action('jh_credit'); ?>
+			</div><!-- .footer_wrap -->
+	
+	</footer><!-- #footer -->
 
 	</div><!-- #container -->
 
