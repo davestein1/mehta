1aef0f353fe005a2ffc62cadd9632c4bd24e4299
David Stein <github@steinbay.com>
2015-07-07 17:51:18 -0700

    primary menu button

--- menu/primary.php
+++ menu/primary.php
@@ -2,6 +2,13 @@
 
 	<nav <?php hybrid_attr( 'menu', 'primary' ); ?>>
 
+		<h3 id="menu-primary-title" class="menu-toggle">
+			<button class="screen-reader-text"><?php
+				/* Translators: %s is the nav menu name. This is the nav menu title shown to screen readers. */
+				printf( _x( '%s Menu', 'nav menu title', 'mehta' ), hybrid_get_menu_location_name( 'primary' ) ); 
+			?></button>
+		</h3><!-- .menu-toggle -->
+
 		<?php wp_nav_menu(
 			array(
 				'theme_location'  => 'primary',
