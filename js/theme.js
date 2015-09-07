8701a303a3b74634f1e5e0dfc50c9476bc9ca941
David Stein <github@steinbay.com>
2015-09-07 10:29:55 -0700

    added smallscreen class for homepage switching

--- js/theme.js
+++ js/theme.js
@@ -1,32 +1,215 @@
 jQuery( document ).ready( function($) {
+
+	/* Catch a small screen and add a body class */
+	if ( window.innerWidth < 600 ) {
+		/* alert("small screen!"); */
+		document.getElementsByTagName('body')[0].className+=' smallscreen'; 
+	} 
+
+	/* == Works Filter Name -- set text and class DS == */
+	if($( ".works-filter" ).is( ":visible" )) {
+		$(".works-filter-toggle").text("Close Filter");
+		$(".works-filter-toggle").addClass("works-filter-open");
+	}else {
+		$(".works-filter-toggle").text("Open Filter");
+		$(".works-filter-toggle").removeClass("works-filter-open")
+	};
 	
-	$("body").addClass( "js-active" );
+	/* == Works Filter toggle == */
+	$( ".works-filter-toggle" ).click( function(e) {
+		
+		/* DS mods swizzle from above. */
+		if($( ".works-filter" ).is( ":visible" )) {
+			$(".works-filter-toggle").text("Open Filter");
+			$(".works-filter-toggle").removeClass("works-filter-open");
+		}else {
+			$(".works-filter-toggle").text("Close Filter");
+			$(".works-filter-toggle").addClass("works-filter-open");
+		};
+
+		e.preventDefault();
+		$( ".works-filter" ).slideToggle();
 	
-	$('img.header-image').addClass( "grayit" ); /* gray header. Removed by theme~footer.js */
-	
+	});
+
 	/* === GRID and TILE View Masonry ===
-		Just turn it off until footer js turns it on. */
-	if ( $("body").hasClass( "col-masonry" ) ) {
-	
+		we receive col-masonry and in turn set col-masonry-active  */
+	if ( $("body").hasClass( "col-masonry" ) ){
+
 		/* add body class */
 		$("body").addClass( "col-masonry-active" );
 
 		/* masonry container */
 		var masonry_container = $('.col-masonry-active .content-entry-wrap');
-		
-		/* Hide the Masonry area on the page until fully loaded. 
-		  theme-footer.js uses imagesLoaded to wait. */
+
+		/* trigger masonry on document ready 
+		masonry_container.masonry(); */
+
 		masonry_container.css({ opacity: 0 });
-	}
+
+		masonry_container.imagesLoaded()
+			.always( function( instance ) {
+				//trigger masonry on document ready
+				masonry_container.masonry();
+				masonry_container.css({ opacity: 1 });
+				//var temp = masonry_container.data('masonry');
+				//alert( temp );
+			    console.log('all images loaded');
+			    //alert("all done img loading");
+			})
+			.done( function( instance ) {
+			    console.log('all images successfully loaded');
+			    //alert("all aok img load. no fails!");
+			})
+			.fail( function() {
+			    console.log('all images loaded, at least one is broken');
+			    alert("at least one img load fail!");
+			})
+			.progress( function( instance, image ) {
+			    //var result = image.isLoaded ? 'loaded' : 'broken';
+			    //console.log( 'image is ' + result + ' for ' + image.img.src );
+		    	//alert( 'image is ' + result + ' for ' + image.img.src );
+			});
+		
+	}; // end class check
 	
-	/* Help the menu fit on one line on screens narrower than 615 pixels. 
-		replace Contact/Publishing with Contact.  */
-	var windowsize = window.innerWidth; 
-	/* alert(windowsize); */
-	if ( windowsize < 761 ) {
-		//$("#menu-secondary #menu-item-26").find("a").text('Contact');
-		$("#menu-item-26 > a").text('Contact');
-	}
+	/* == Switch To List View == */
+	$( ".works-list-toggle" ).click( function(e) {
+		e.preventDefault();
+		$( "body" ).removeClass("col-grid col-tile");
+		$( "body" ).addClass("col-list");
+		var masonry_container = $('.col-masonry-active .content-entry-wrap');
+		/* work around list view bug with double call */
+		setTimeout(function() {
+			masonry_container.masonry();
+		}, 1000);
+		masonry_container.masonry();
+	});
+
+	/* == Switch To Grid View == */
+	$( ".works-grid-toggle" ).click( function(e) {
+		e.preventDefault();
+		$( "body" ).removeClass("col-list col-tile");
+		$( "body" ).addClass("col-grid");
+		//alert("col-grid");
+		var masonry_container = $('.col-masonry-active .content-entry-wrap');
+		masonry_container.masonry();
+	});
+
+	/* == Switch To Tile View == */
+	$( ".works-tile-toggle" ).click( function(e) {
+		e.preventDefault();
+		$( "body" ).removeClass("col-list col-grid");
+		$( "body" ).addClass("col-tile");
+		var masonry_container = $('.col-masonry-active .content-entry-wrap');
+		masonry_container.masonry();
+	});
+
+	/* Deprecated. Works.
+	$('a[rel="smoothscroll"]').click(function(){
+		$('#scrollwindow').animate({ 
+			scrollTop:  $( $.attr(this, 'href') ).offset().top - $('#one').offset().top
+		}, 2000); 
+		return false;
+	}); */
 	
-	console.log('hello from theme.js. Screen width is ' + windowsize + 'px.');
+	/* For snowfall page scrolling. */
+	$('a[rel="smoothscroll_down"]').click(function(){
+		
+		$( "body" ).addClass("smoothscroll_used");
+		
+		var offset_top = $('#scrollwindow').offset().top + 20;
+		var scrolled_from_top = $('#scrollwindow').scrollTop();
+		var scroll_time = 2000; /* Default scroll time 2 seconds */
+		
+		var index_max = $('.smoothscroll_stop').length - 1
+		 
+		 /* $( $('.smoothscroll_stop').get().reverse() )
+		 	.each(function( index, scroll_stop ) { */
+		 $('.smoothscroll_stop').each(function( index, scroll_stop ) { 
+		
+			var stop_offset = $(scroll_stop).offset().top - offset_top + scrolled_from_top;
+			
+			/*alert(index + " scrolled:" + scrolled_from_top + " stop_offset:" + stop_offset + " offset:" + offset_top + " " + index_max); */
+			
+			if ( stop_offset - scrolled_from_top > 9 ) {
+				
+				$('#scrollwindow').animate({ 
+					scrollTop:  stop_offset 
+				}, scroll_time );
+				
+				return false; 
+				
+			}
+
+			/* At the end? Scroll back to top */
+			if ( index == index_max ) { 
+				
+				stop_offset = 0; 
+				scroll_time = 4000;
+				
+				$('#scrollwindow').animate({ 
+					scrollTop:  stop_offset 
+				}, scroll_time );
+				
+				return false; 
+				
+			}
+			
+		});
+
+		return false;
+	});
+	
+	/* For snowfall page scrolling. */
+	$('a[rel="smoothscroll_up"]').click(function(){
+		
+		$( "body" ).addClass("smoothscroll_used");
+		
+		var scroll_time = 2000; /* Default scroll time 2 seconds */
+		var offset_top = $('#scrollwindow').offset().top + 20;
+		var scrolled_from_top = $('#scrollwindow').scrollTop();
+		
+		 $( $('.smoothscroll_stop').get().reverse() ).each(function( index, scroll_stop ) { 
+		 /* $('.smoothscroll_stop').each(function( index, scroll_stop ) { */
+		
+			var stop_offset = $(scroll_stop).offset().top - offset_top + scrolled_from_top;
+			
+			/* alert(index + " scrolled:" + scrolled_from_top + " stop_offset:" + stop_offset + " offset:" + offset_top + " " ); */
+			
+			if ( scrolled_from_top - stop_offset > 9 ) {
+				
+				$('#scrollwindow').animate({ 
+					scrollTop:  stop_offset 
+				}, scroll_time );
+				
+				return false; 
+				
+			}
+		
+		});
+		
+		return false;
+	});
+
+	/* Responsive menus have toggles. We have combined them. 
+		HERE WE TOGGLE BOTH PRIMARY AND SECONDARY MENUS */
+	$( '.menu-toggle button' ).click(
+		function() {
+			//$( this ).parents( '.menu' ).children( '.wrap' ).fadeToggle();
+			//$( this ).toggleClass( 'active' );
+			window.scrollTo( 0, 0 );
+			$( '.menu-toggle button' ).parents( '.menu' ).children( '.wrap' ).fadeToggle();
+			$( '.menu-toggle button' ).toggleClass( 'active' );
+		}
+	);
+	
+	/* Banner loads as grayscale. Color as signal we have fully loaded. */
+	$( window ).load( function() {
+		//alert("hello from the fully loaded page and theme-footer.js"); 
+		$('img.header-image').removeClass( "grayit" );
+	});
+	
+	console.log('hello from theme-footer.js');
+
 });
